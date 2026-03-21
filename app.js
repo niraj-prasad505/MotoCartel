5ff8a837bdf3631cac77c1acb7dd028878e0b02b
niraj prasad <nirajprasad505@gmail.com>
2026-03-21 14:28:18 +0530

    edit app,js

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const app = express();
 
-const cookiparser = requre("cooki-parser");
+const cookieParser = require("cookie-parser");
 const path = require("path");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(cookiparser());
-app.use(express.static(path,join(__dirname,"public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("view engine", "ejs");
 
-app,get("/",(req, res)=>{
+
+app.get("/", (req, res) => {
     res.send("hey");
 });
 
-app.listen(3000);
-
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
