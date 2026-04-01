742c21eb55e59e978147b1981c6137ca7a40230f
niraj prasad <nirajprasad505@gmail.com>
2026-04-01 19:12:28 +0530

    fix nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,24 +1,45 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/image.png";
+import cartlogo from "../assets/cartlogo.png";
+import userlogo from "../assets/userlogo.png";
+
 
 const Navbar = () => {
   return (
-    <nav>
-      <h2>MotoCartel</h2>
+    <nav className="flex items-center px-6 py-3">
+      {/* Left */}
 
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/products">Products</Link>
-        <Link to="/cart">Cart</Link>
-        <Link to="/login">Login</Link>
+      <img src={logo} alt="logo" className="w-30 h-20 object-contain" />
+
+      {/* Center */}
+      <div className="flex gap-20 mx-auto text-lg font-semibold tracking-wide text-gray-700">
+        <Link to="/">HOME</Link>
+        <Link to="/products">PRODUCT</Link>
+        <Link to="/cart">CART</Link>
+        <Link to="/login">LOGIN</Link>
       </div>
-      <div>
-        <div>cart</div>
-        <div>login</div>
-        <div>menu</div>
-        <h1 className="text-red-500 text-2xl">Test</h1>
-        <h1 className="text-red-500 text-2xl">Working</h1>
-        <h1 className="text-red-500 text-2xl">Working</h1>
-        <h1 className="text-red-500 text-2xl">Working</h1>
+
+      {/* Right */}
+      <div className="flex items-center gap-11">
+
+        {/* Cart Icon */}
+        <div className="w-12 h-12 border-1 flex items-center justify-center rounded-full bg-gray-300 cursor-pointer">
+          <img src={cartlogo} alt="cartlogo" className="w-10 h-6 object-contain" />
+        </div>
+
+        {/* Login Pill */}
+        <div className="flex items-center gap-4 px-6 bg-[#212121] text-white rounded-full cursor-pointer text-xl h-13">
+          <span>Login</span>
+          <span>
+            <img src={userlogo} alt="logo" className="w-10 h-20 object-contain" />
+          </span>
+        </div>
+
+        {/* Menu Icon */}
+        <div className="w-6 h-6 flex items-center justify-center cursor-pointer">
+          ☰
+        </div>
+
       </div>
     </nav>
   );
