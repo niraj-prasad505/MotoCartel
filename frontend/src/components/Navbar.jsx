7d43960b5216993a379bb7b999aedcfac690f575
niraj prasad <nirajprasad505@gmail.com>
2026-04-12 09:36:17 +0530

    nav created

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,31 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/image.png";
-import cartlogo from "../assets/cartlogo.png";
+import logo from "../assets/logo.png";
 import userlogo from "../assets/userlogo.png";
-
+import { Search } from "lucide-react";
+import { Heart } from "lucide-react";
+import { ShoppingBag } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center px-6 py-3">
+    <nav className="flex items-center px-6 py-3 bg-gradient-to-r from-[#0f172a] to-[#1e293b] h-24">
       {/* Left */}
 
-      <img src={logo} alt="logo" className="w-30 h-20 object-contain" />
+      <img src={logo} alt="logo" className="w-45 h-20 object-contain" />
+
+      <div className="flex items-center bg-[#0b1220] rounded-full px-4 py-2 w-[350px] ml-[20px]
+border border-gray-500 focus-within:border-white focus-within:ring-1 focus-within:ring-white transition">
+
+        <input
+          type="text"
+          placeholder="Search products..."
+          className="bg-transparent outline-none text-gray-300 w-full placeholder-gray-500"
+        />
+
+        <Search className="text-gray-400 w-5 h-5 cursor-pointer " />
+      </div>
 
       {/* Center */}
-      <div className="flex gap-20 mx-auto text-lg font-semibold tracking-wide text-gray-700">
+      <div className="flex gap-14 mx-auto text-base font-semibold tracking-wide text-white">
         <Link to="/">HOME</Link>
         <Link to="/products">PRODUCT</Link>
         <Link to="/cart">CART</Link>
@@ -20,24 +33,26 @@ const Navbar = () => {
       </div>
 
       {/* Right */}
-      <div className="flex items-center gap-11">
+      <div className="flex items-center gap-6">
 
         {/* Cart Icon */}
-        <div className="w-12 h-12 border-1 flex items-center justify-center rounded-full bg-gray-300 cursor-pointer">
-          <img src={cartlogo} alt="cartlogo" className="w-10 h-6 object-contain" />
+        <div className="w-11 h-11 flex items-center justify-center rounded-full bg-[#0b1220] ring-1 ring-gray-600 shadow-[0_0_20px_rgba(255,255,255,0.08)] cursor-pointer">
+
+          <ShoppingBag className="w-6 h-6 text-gray-300" />
+        </div>
+
+        {/* Cart Icon */}
+        <div className="w-11 h-11 flex items-center justify-center rounded-full bg-[#0b1220] ring-1 ring-gray-600 shadow-[0_0_20px_rgba(255,255,255,0.08)] cursor-pointer">
+
+          <Heart className="w-6 h-6 text-gray-300" />
         </div>
 
         {/* Login Pill */}
-        <div className="flex items-center gap-4 px-6 bg-[#212121] text-white rounded-full cursor-pointer text-xl h-13">
-          <span>Login</span>
+        <div className="flex items-center gap-4 px-6 bg-[#212121] text-white rounded-full cursor-pointer text-base h-11">
+          <span>Login/create</span>
           <span>
             <img src={userlogo} alt="logo" className="w-10 h-20 object-contain" />
           </span>
-        </div>
-
-        {/* Menu Icon */}
-        <div className="w-6 h-6 flex items-center justify-center cursor-pointer">
-          ☰
         </div>
 
       </div>
