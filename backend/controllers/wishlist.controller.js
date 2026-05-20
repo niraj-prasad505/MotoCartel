f7178053045c533be25e5dd179b7599089232366
niraj prasad <nirajprasad505@gmail.com>
2026-05-20 14:48:02 +0530

    fix

---
+++ backend/controllers/wishlist.controller.js
@@ -0,0 +1,16 @@
+const User = require("../models/user-models");
+
+const addToWishlist = async (req, res) => {};
+
+const removeFromWishlist = async (req, res) => {};
+
+const moveToCart = async (req, res) => {};
+
+const getWishlist = async (req, res) => {};
+
+module.exports = {
+  addToWishlist,
+  removeFromWishlist,
+  moveToCart,
+  getWishlist
+};
---
+++ backend/routes/wishlistRouter.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const { protect } = require("../middlewares/auth.middleware");
+const {
+  addToWishlist,
+  removeFromWishlist,
+  moveToCart,
+  getWishlist
+} = require("../controllers/wishlist.controller");
+
+router.post("/wishlist/add", protect, addToWishlist);
+router.delete("/wishlist/remove/:productId", protect, removeFromWishlist);
+router.post("/wishlist/move-to-cart", protect, moveToCart);
+router.get("/wishlist", protect, getWishlist);
