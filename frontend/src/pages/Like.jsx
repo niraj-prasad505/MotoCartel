c90a8e78f187f407b54e9b07039aa02c48a7d107
niraj prasad <nirajprasad505@gmail.com>
2026-05-25 08:03:34 +0530

    fix

--- frontend/src/pages/Like.jsx
+++ frontend/src/pages/Like.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Like() {
   return (
-    <div className='h-6 bg-green-400'>likeshbcsjhbcsjcbcb</div>
+    <div className='h-6 bg-green-500'>likeshbcsjhbcsjcbcb</div>
   )
 }
 
