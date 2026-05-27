fe79fbf4c25a25a7853aa8e1f579fa686494737f
niraj prasad <nirajprasad505@gmail.com>
2026-05-27 08:36:30 +0530

    fix fetchwishlist

--- frontend/src/pages/Like.jsx
+++ frontend/src/pages/Like.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import { getWishlistData } from "../services/wishlist";
+import { useState,useEffect } from 'react';
 export default function Like() {
+  const [items, setItems] = useState([]);
+
+  const fetchwishlist = async () => {
+    try {
+      const { data } = await getWishlistData();
+      setItems(data.wish || []);
+      console.log(data)
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchwishlist();
+  }, []);
+  
+
   return (
     <div className='h-6 bg-green-500'>likeshbcsjhbcsjcbcb</div>
+    
   )
 }
 
