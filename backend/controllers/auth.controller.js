5da2c834425ae6def3c05032f8f8dde3f23f6462
niraj prasad <nirajprasad505@gmail.com>
2026-07-01 19:58:05 +0530

    fix

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -22,6 +22,7 @@ const register = async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
 
+    // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
 
