8adede4438c67e4027e7d0f77d043c64719eb42c
niraj prasad <nirajprasad505@gmail.com>
2026-07-06 19:26:54 +0530

    fix

--- frontend/src/components/Checkout/AddressSection.jsx
+++ frontend/src/components/Checkout/AddressSection.jsx
@@ -74,6 +74,7 @@ const AddressSection = ({ address, setAddress, next, saveAddress }) => {
                             </span>
                         </div>
                     </div>
+                    {/* dsdadad */}
                     <div className="flex justify-between items-center pt-8">
                         <h1 className="text-gray-600">if the address is wrong then the product is not delevered</h1>
                         <button onClick={handleContinue} className="bg-orange-500 px-6 py-3 rounded-full align-middle text-lg font-semibold justify-around flex items-center">
