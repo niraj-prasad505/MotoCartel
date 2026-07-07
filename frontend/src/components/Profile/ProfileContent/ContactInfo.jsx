0eb0291ebc996ddb8c27f35d6b78fa20aeec08ee
niraj prasad <nirajprasad505@gmail.com>
2026-07-07 21:41:34 +0530

    feat: add contact, picture, and gender fields to user registration

--- frontend/src/components/Profile/ProfileContent/ContactInfo.jsx
+++ frontend/src/components/Profile/ProfileContent/ContactInfo.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 
 export default function ContactInfo({ userPhone, useremail }) {
+  console.log("userPhone:", userPhone);
   return (
     <AccordionSection title="Contact Info ">
       
