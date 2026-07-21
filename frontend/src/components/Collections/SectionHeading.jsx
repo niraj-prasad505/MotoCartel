862ceb15459f6765eb24b9e1aeb37b85f05d16c9
niraj prasad <nirajprasad505@gmail.com>
2026-07-21 22:07:13 +0530

    fix

--- frontend/src/components/Collections/SectionHeading.jsx
+++ frontend/src/components/Collections/SectionHeading.jsx
@@ -4,7 +4,7 @@ import TrendingProducts from "./TrendingProducts/TrendingProducts";
 import BrandShowcase from "./Brands/BrandShowcase";
 
 export default function SectionHeading({ banners, products}) {
-  // console.log(banners)
+  console.log(banners)
   return (
     <>
       <HeroBanner banners={banners} />
