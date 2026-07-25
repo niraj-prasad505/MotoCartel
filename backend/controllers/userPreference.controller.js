e12d2cf6cf7bc7bf9d8a8ab94ad7b4c15c5bd427
niraj prasad <nirajprasad505@gmail.com>
2026-07-25 12:01:23 +0530

    refactor: improve formatting and structure of updateUserPreference controller

--- backend/controllers/userPreference.controller.js
+++ backend/controllers/userPreference.controller.js
@@ -4,31 +4,32 @@ const UserPreference = require("../models/UserPreference-model");
 const Product = require("../models/product-model");
 const User = require("../models/user-models");
 
-const updateUserPreference = async (req, res) => {
-    try {
-        const userId = req.user.id;
-        const { productId } = req.body;
-        // console.log(userId);
-        // console.log(productId);
-        const userPreference = await UserPreference.findOne({
-            user: userId
-        });
-        console.log(userPreference);
+    const updateUserPreference = async (req, res) => {
+        try {
+            const userId = req.user.id;
+            const { productId } = req.body;
+            // console.log(userId);
+            // console.log(productId);
+            const userPreference = await UserPreference.findOne({
+                user: userId
+            });
+            console.log(userPreference);
+            const product = await Product.findById(productId);
+            console.log(product);
 
 
+            res.status(200).json({
+                success: true,
+                message: "Preference updated"
+            });
 
-        res.status(200).json({
-            success: true,
-            message: "Preference updated"
-        });
-
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            message: err.message
-        });
-    }
-};
+        } catch (err) {
+            res.status(500).json({
+                success: false,
+                message: err.message
+            });
+        }
+    };
 
 module.exports = {
     updateUserPreference
